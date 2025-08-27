98f05ab507fa4dd090ef73923cce63eccfd9080b
Gyl Monteiro <gyldeveloper@gmail.com>
2025-08-27 09:50:22 -0300

    remove a pasta de migrações

--- prisma/migrations/20250826131022_cria_tabela_turma/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."turma" (
-    "id" SERIAL NOT NULL,
-    "nome" TEXT NOT NULL,
-    "turno" TEXT NOT NULL,
-
-    CONSTRAINT "turma_pkey" PRIMARY KEY ("id")
-);
